8725aa2ceba5856ecf39a60ca7d0fe7b44dffab2
Yauheni Shaikouski <yauheni.shaikouski@elinext.com>
2024-01-28 18:06:50 +0300

    add https support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'fs';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: readFileSync(join(__dirname, '..', 'cert/key.pem')),
+    cert: readFileSync(join(__dirname, '..', 'cert/cert.pem')),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT);
 }
